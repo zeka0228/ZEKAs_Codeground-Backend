d0b064d68319d24ea2646dbe6b72c9bb20a77c81
zeka0228 <jhim0228@gmail.com>
2025-06-29 20:46:08 +0900

    :sparkles: [Feat] mmr과 관련 변수 업데이트 함수 추가 및 mmr_measure 내 오타 수정

--- src/app/domain/user/crud/user_crud.py
+++ src/app/domain/user/crud/user_crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from src.app.models.models import User
+from typing import  Optional
 
-
-async def get_user_by_id(db: Session, input_id: int):
+async def get_user_by_id(db: Session, input_id: int) -> Optional[User]:
     return db.query(User).filter(User.user_id == input_id).first()
--- src/app/models/models.py
+++ src/app/models/models.py
@@ -1,7 +1,7 @@
-from src.app.domain.match.utils.mmr_measure import RD_MAX, SIGMA_DEF
-from sqlalchemy import Column, DateTime, Integer, String, func, Text, Float, ForeignKey
 from src.app.core.database import Base
-
+from src.app.domain.match.utils.mmr_measure import RD_MAX, SIGMA_DEF
+from sqlalchemy import Column, DateTime, Integer, String, func, Text, Float, ForeignKey, Enum, Boolean
+from enum import Enum as PyEnum
 
 class User(Base):
     __tablename__ = "users"
@@ -18,8 +18,6 @@ class User(Base):
     my_tier = Column(Float, nullable=False, server_default="1000")  # 내 티어
     profile_img_url = Column(Text, nullable=True)  # 프로필 이미지 주소
 
-
-# mmr.user_mmr  ← MMR 전용 스키마
 class UserMmr(Base):
     __tablename__ = "user_mmr"
     __table_args__ = {"schema": "mmr"}  # 스키마 분리
@@ -29,3 +27,25 @@ class UserMmr(Base):
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
     rating_devi = Column(Integer, nullable=False, server_default=str(RD_MAX))
     volatility = Column(Float, nullable=False, server_default=str(SIGMA_DEF))
+
+
+class MatchResult(PyEnum):
+    WIN  = "win"
+    DRAW = "draw"
+    LOSS = "loss"
+
+
+class MatchLog(Base):
+    __tablename__ = "match_log"
+    __table_args__ = {"schema": "match_log"}
+    match_log_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("public.users.user_id", ondelete="CASCADE"), index=True)
+    match_id = Column(Integer, ForeignKey("public.matches.match_id", ondelete="CASCADE"))
+    #problem_id = Problem 테이블 완성 시 설정
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    result = Column(Enum(MatchResult, name="match_result"), nullable=False)
+    mmr_earned = Column(Float, nullable=False, server_default="0" ) # 경기 종료 -> log 기록 -> 유저 mmr 업데이트하면서 동시에 업데이트
+    opponent_mmr = Column(Float, nullable=False)
+    opponent_rd = Column(Float, nullable=False)
+    is_consumed = Column(Boolean, nullable=False, server_default="False",index=True)
+
